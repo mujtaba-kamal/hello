381a572d0cf1207a0fa6b3c5280f1790bb8934e0
mujtaba-kamal <66316181+mujtaba-kamal@users.noreply.github.com>
2020-06-12 13:29:18 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<div >
+  hello world! 
+</div>,
   document.getElementById('root')
 );
 
